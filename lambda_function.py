65cd55d11687c74feb7d21ccf4002ab5e64d2426
MizushimaKeita <mizushima.keita@lancers.co.jp>
2019-06-05 19:18:04 +0900

    lambdaから環境変数を読み込む部分を追記

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,13 @@
 # 並列処理
 import threading
+import os
 
+# 検索クエリ
+query = os.environ["query"]
+# ネガポジ閾値
+negpos_threshold = int(os.environ["negpos_threshold"])
+# Slack通知先
+slack_room = os.environ["slack_room"]
 
 def lambda_function(event, content):
     thread_1 = threading.Thread(target=return_200)
