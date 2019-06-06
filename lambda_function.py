c9683b99819459ff7748342868654b2355e0de9f
MizushimaKeita <mizushima.keita@lancers.co.jp>
2019-06-06 17:03:19 +0900

    とりあえず検索結果をとってくる部分を十そう

--- lambda_function.py
+++ lambda_function.py
@@ -2,12 +2,21 @@
 import threading
 import os
 
+from requests_oauthlib import OAuth1Session  # OAuthのライブラリの読み込み
+
 # 検索クエリ
 query = os.environ["query"]
 # ネガポジ閾値
 negpos_threshold = int(os.environ["negpos_threshold"])
 # Slack通知先
-slack_room = os.environ["slack_room"]
+slack_room = os.environ["slack_chanel"]
+
+# twitterAPIアクセストークン
+CK = os.environ["twitter_CK"]
+CS = os.environ["twitter_CS"]
+AT = os.environ["twitter_AT"]
+ATS = os.environ["twitter_ATS"]
+
 
 def lambda_function(event, content):
     thread_1 = threading.Thread(target=return_200)
@@ -20,4 +29,12 @@ def return_200():
     return 0
 
 def main_func(event, content):
-    return event
+    twitter = OAuth1Session(CK, CS, AT, ATS)  #認証処理
+
+    # twitter検索結果取得エンドポイント
+    url = 'https://api.twitter.com/1.1/search/tweets.json'
+
+    # ランサーズが含まれる昨日からのツイートを取得する
+    res = twitter.get(url, params={'q': query})
+    print(res)
+    return
